63ec39f14ee565651044f39b26007d45862d1176
std67122420321-crypto <std.67122420321@ubru.ac.th>
2025-12-09 09:48:57 +0700

    first commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return f'<h1>Hello world!</h1>'
+
+@app.route('/name')
+def name():
+    return f'<h1>Sitthinonkongton</h1>'
+
+# if __name__=='__main__':
+#     app.run(debug=True)
