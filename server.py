60d9ce98ea7517698b473a2456670ac3e24f1ee8
std67122420321-crypto <std.67122420321@ubru.ac.th>
2025-12-09 10:40:40 +0700

    add multiple routes

--- server.py
+++ server.py
@@ -1,14 +1,53 @@
 from flask import Flask
+import uuid
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'<h1>Hello world!</h1>'
+        return f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <h1>Home Page</h1>
+      <hr
+    </body>
+    </html>
+"""
 
 @app.route('/name')
 def name():
-    return f'<h1>Sitthinonkongton</h1>'
+    return f'<h1>Sitthinon Kongton</h1>'
+
+@app.route('/user/<username>')
+def user(username):
+    return f'<h1>My name is: {username}</h1>'
+
+@app.route('/calculator/addition/<int:a>/<int:b>')
+def addition(a, b):
+  return f'<h1>{a}  {b} = {a + b}</h1>'
+
+@app.route('/calculator/subtraction/<int:a>/<int:b>')
+def subtraction (a, b):
+  return f'<h1>{a} - {b} = {a - b}</h1>'
+
+@app.route('/calculator/multiplication/<int:a>/<int:b>')
+def multiplication(a, b):
+  return f'<h1>{a} * {b} = {a *  b}</h1>'
+
+@app.route('/calculator/division/<int:a>/<int:b>')
+def division(a, b):
+  return f'<h1>{a} / {b} = {a / b}</h1>'
+
+@app.route('/secretkey/<uuid:sk>')
+def my_secret_key(sk):
+  return f'<h1>My secret key is: {sk}</h1>'
+
 
 # if __name__=='__main__':
 #     app.run(debug=True)
